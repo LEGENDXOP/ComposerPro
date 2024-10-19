e10d0af4a8738043773bfa8d0532117d5b5483d7
LEGEND X <legendxx08377@gmail.com>
2024-10-19 16:38:15 +0530

    again

--- ComposerPro/build.gradle.kts
+++ ComposerPro/build.gradle.kts
@@ -39,7 +39,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.legendxop"
             artifactId = "composer-pro"
-            version = "1.0.0"
+            version = "1.0.1"
 
             afterEvaluate {
                 from(components["release"])
